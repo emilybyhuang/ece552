52750ffada4eeb3043766d08c20c03b562b3b55d
Emily Huang <emily.hby@gmail.com>
2022-10-16 20:35:23 -0400

    Done first 2

--- cbp4-assign2/predictor.cc
+++ cbp4-assign2/predictor.cc
@@ -1,62 +1,103 @@
 #include "predictor.h"
-
 /////////////////////////////////////////////////////////////
 // 2bitsat
 /////////////////////////////////////////////////////////////
 
 // array to hold all of the prections
 // since prediction tables have 8192 bits, each entry is 2 bits => 8192/2=4096
-#define NUM_PRED_TWOBIT_SAT 4096
+#define NUM_ENTRIES_TWOBIT_SAT 4096
 // 4096 is 2 ^ 12  thus use this bit mask to get lowest 12 bits of pc
 #define TWELVE_BIT_MASK 0xFFF 
 
-UINT32 predictionTable[NUM_PRED_TWOBIT_SAT];
+
+UINT32 predictionTable_2bitsat[NUM_ENTRIES_TWOBIT_SAT];
 
 void InitPredictor_2bitsat() {
-	std::fill_n(predictionTable, NUM_PRED_TWOBIT_SAT, 0b01);	
+	// init all entries to weak not-taken
+	std::fill_n(predictionTable_2bitsat, NUM_ENTRIES_TWOBIT_SAT, 0b01);	
 }
 
 bool GetPrediction_2bitsat(UINT32 PC) {
 	//get the lowest 12 bits to index into the global array
-	UINT32 pcForIndex = PC & TWELVE_BIT_MASK;
-	UINT32 prediction = predictionTable[pcForIndex];
+	UINT32 index = PC & TWELVE_BIT_MASK;
+	UINT32 prediction = predictionTable_2bitsat[index];
 	// if second lowest bit is 1: TAKEN	
-	if(prediction >> 1)return TAKEN;
-	else return NOT_TAKEN;
+	if(prediction >> 1)
+		return TAKEN;
+	else
+		return NOT_TAKEN;
+}
+
+void update_2bitsat(bool resolveDir, bool predDir, UINT32 * table, UINT32 index){
+	if(predDir == NOT_TAKEN){
+		if(resolveDir == TAKEN)
+			table[index]++;
+		else	
+			table[index] = 0b00;
+	}else{
+		if(resolveDir == TAKEN)
+			table[index] = 0b11;
+		else
+			table[index]--;	
+	}	
 }
 
 void UpdatePredictor_2bitsat(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget) {
 	// get the lowest 12 bit to index into global array
-	UINT32 pcForIndex = PC & TWELVE_BIT_MASK;
-
-	if(predDir == NOT_TAKEN){
-		if(resolveDir == TAKEN)
-			predictionTable[pcForIndex]++;
-		else	
-			predictionTable[pcForIndex] = 0b00;
-	}else{
-		if(resolveDir == TAKEN)
-			predictionTable[pcForIndex] = 0b11;
-		else
-			predictionTable[pcForIndex]--;	
-	}	
+	UINT32 index = PC & TWELVE_BIT_MASK;
+	update_2bitsat(resolveDir, predDir, predictionTable_2bitsat, index);
 }
 
 /////////////////////////////////////////////////////////////
 // 2level
 /////////////////////////////////////////////////////////////
 
-void InitPredictor_2level() {
+#define NUM_ENTRIES_PRIVATE_HISTORY_TABLE 512
+#define NUM_PRIVATE_PREDICTOR_TABLES 8
+#define NUM_ENTRIES_PRIVATE_PREDICTOR_TABLE 64
 
+#define THREE_BIT_MASK 0x7
+#define SIX_BIT_MASK 0x3F
+#define NINE_BIT_MASK 0XFF8
+
+UINT32 privateHistoryTable_2level[NUM_ENTRIES_PRIVATE_HISTORY_TABLE];
+UINT32 privatePredictorTable_2level[NUM_PRIVATE_PREDICTOR_TABLES][NUM_ENTRIES_PRIVATE_PREDICTOR_TABLE];
+
+void InitPredictor_2level() {
+	// init all history to 0
+	std::fill_n(privateHistoryTable_2level, NUM_ENTRIES_PRIVATE_HISTORY_TABLE, 0);	
+
+	// init all entries to weak not-taken
+	for(int i = 0; i < NUM_PRIVATE_PREDICTOR_TABLES; i++){
+		std::fill_n(privatePredictorTable_2level[i], NUM_ENTRIES_PRIVATE_PREDICTOR_TABLE, 0b01);	
+	}
 }
 
 bool GetPrediction_2level(UINT32 PC) {
+	UINT32 bht_index = PC & NINE_BIT_MASK;
+	bht_index = bht_index >> 3;
+	UINT32 pht_index = PC & THREE_BIT_MASK;
 
-  return TAKEN;
+	UINT32 history = privateHistoryTable_2level[bht_index] & SIX_BIT_MASK;
+	UINT32 prediction = privatePredictorTable_2level[pht_index][history];
+	if(prediction >> 1)
+		return TAKEN;
+	else
+  		return NOT_TAKEN;
 }
 
 void UpdatePredictor_2level(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget) {
+	UINT32 bht_index = PC & NINE_BIT_MASK;
+	bht_index = bht_index >> 3;
+	UINT32 pht_index = PC & THREE_BIT_MASK;
 
+	UINT32 history = privateHistoryTable_2level[bht_index] & SIX_BIT_MASK;
+
+	// update predictor table
+	update_2bitsat(resolveDir, predDir, privatePredictorTable_2level[pht_index], history);
+
+	// update history
+	privateHistoryTable_2level[bht_index] = privateHistoryTable_2level[bht_index] << 1 | resolveDir;
 }
 
 /////////////////////////////////////////////////////////////
