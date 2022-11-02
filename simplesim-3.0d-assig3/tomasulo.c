b00a3fabe63fdec7867abd5ece508bbda7f06fa9
Emily Huang <emily.hby@gmail.com>
2022-11-02 13:20:51 -0400

    I can't issue if I only have rs entry ready: still need to check FU

--- simplesim-3.0d-assig3/tomasulo.c
+++ simplesim-3.0d-assig3/tomasulo.c
@@ -33,6 +33,9 @@
 #define FU_INT_LATENCY     5
 #define FU_FP_LATENCY      7
 
+#define NUM_INPUT_REGS     3
+#define NUM_OUTPUT_REGS    2
+
 /* IDENTIFYING INSTRUCTIONS */
 
 //unconditional branch, jump or call
@@ -213,7 +216,14 @@ void execute_To_CDB(int current_cycle) {
  */
 void issue_To_execute(int current_cycle) {
 
-  /* ECE552: YOUR CODE GOES HERE */
+   /* ECE552: YOUR CODE GOES HERE */
+   // Check all of the reservation stations and execute one from INT and FP
+   // if all of their operands are ready
+
+
+
+   
+
 }
 
 bool getFreeReservationEntry(instruction_t *reservationStationTable[], int size, instruction_t *insertPlace){
@@ -227,6 +237,31 @@ bool getFreeReservationEntry(instruction_t *reservationStationTable[], int size,
    return true;
 }
 
+bool operandsReady(instruction_t *entry){
+   for(int i = 0; i < NUM_INPUT_REGS; i++){
+      if(entry -> Q[i] != NULL)
+         return false;
+   }
+   return true;
+}
+
+// issue the first that has been dispatched with all operands ready in reservation station
+instruction_t *t getOldestRsToIssue(instruction_t *rsTable[], int rsTableSize, int current_cycle){
+   int oldest_cycle = current_cycle;
+   instruction_t *oldestRSEntry = NULL; 
+   for(int i = 0; i < rsTableSize; i++){
+      if(rsTable[i] == NULL)
+         continue;
+      // if all operands ready and dispatch cycle is before current cycle: candidate for issue
+      if(operandsReady(rsTable[i]) && 
+         rsTable[i] -> tom_dispatch_cycle < oldest_cycle){
+         oldest_cycle = tom_dispatch_cycle;
+         oldestRSEntry = rsTable[i];
+      }  
+   }
+   return oldestRSEntry;
+}
+
 /* 
  * Description: 
  * 	Moves instruction(s) from the dispatch stage to the issue stage
@@ -235,60 +270,28 @@ bool getFreeReservationEntry(instruction_t *reservationStationTable[], int size,
  * Returns:
  * 	None
  */
+
+// ENTER ISSUE STAGE
+// Main purpose: check if issuable: if source operands are ready(Q is all NULL)
+// can issue
 void dispatch_To_issue(int current_cycle) {
 
    /* ECE552: YOUR CODE GOES HERE */
-   // case where no instruction in instruction queue
-   if (IFQ -> head == IFQ -> tail){
-      return;
+   // If no RAW: issue
+
+   // check reservINT: get the oldest one
+   instruction_t *reservIntEntryToIssue = getOldestRsToIssue(reservINT, RESERV_INT_SIZE, current_cycle);
+   // NULL means that there's nothing ready to issue
+   if(reservIntEntryToIssue){
+      reservIntEntryToIssue.tom_issue_cycle = current_cycle;
    }
- 
 
-   /*
-   A fetched instruction can be dispatched (complete D) immediately if a reservation
-   station entry will be available in the next cycle. Structural hazards on reservation
-   stations are resolved here with stalls.
-   */
-   bool D_success = false;
-   instruction_t* currInstr = IFQ->head->data;
-   // check for structural hazard
-
-   md_opcode currOp = currInstr->op;
-
-   //OoO issue
-   //check from head to tail: the first available dispatched && has RS available
-
-
-   if(IS_UNCOND_CTRL(currOp) || IS_COND_CTRL(currOp)){
-      D_success = true;
-      curr_insn->tom_dispatch_cycle = current_cycle;
-   }else if (USES_INT_FU(currOp)) {
-      //INT
-      instruction_t *insertPlace = NULL;
-      // returns true if avail entry and entryToInsert is where to insert
-      // returns false if no entry avail
-      D_success = getFreeReservationEntry(reservINT, RESERV_INT_SIZE, insertPlace);
-      if(D_success){
-         insertPlace = currInstr;
-         curr_insn->tom_dispatch_cycle = current_cycle;
-      }
-   } else if (USES_FP_FU(currOp)) {
-      //FP
-      instruction_t *insertPlace = NULL;
-      // returns true if avail entry and entryToInsert is where to insert
-      // returns false if no entry avail
-      D_success = getFreeReservationEntry(reservFP, RESERV_FP_SIZE, insertPlace);
-      if(D_success){
-         insertPlace = currInstr;
-         curr_insn->tom_dispatch_cycle = current_cycle;
-      } 
-   }
-   
-   // remove instruction from queue
-   if(D_success)
-      queuePop(IFQ);
-   // if !D_success: stall so don't pop from queue cuz wanna retry when reservation
-   // station needed is ready
+   // check reservFP: get the oldest one
+   instruction_t *reservFpEntryToIssue = getOldestRsToIssue(reservFP, RESERV_FP_SIZE, current_cycle);
+   // NULL means there's nothing ready to issue
+   if(reservFpEntryToIssue){
+      reservFpEntryToIssue.tom_issue_cycle = current_cycle;
+   }  
 }
 
 /* 
@@ -299,26 +302,12 @@ void dispatch_To_issue(int current_cycle) {
  * Returns:
  * 	None
  */
+
+// ENTER FETCH STAGE
+// Main purpose:  fetch instr and push into IFQ queue
 void fetch(instruction_trace_t* trace) {
   
-  /* ECE552: YOUR CODE GOES HERE */
-   
-}
-
-
-/* 
- * Description: 
- * 	Calls fetch and dispatches an instruction at the same cycle (if possible)
- * Inputs:
- *      trace: instruction trace with all the instructions executed
- * 	current_cycle: the cycle we are at
- * Returns:
- * 	None
- */
-void fetch_To_dispatch(instruction_trace_t* trace, int current_cycle) {
-  /* ECE552: YOUR CODE GOES HERE */
-
-
+   /* ECE552: YOUR CODE GOES HERE */
    if (fetch_index >= INSTR_TRACE_SIZE || isQueueFull(IFQ)) {
       return;
    }
@@ -341,6 +330,107 @@ void fetch_To_dispatch(instruction_trace_t* trace, int current_cycle) {
    if(!instrInsertionSuccess){
       fetch_index--;
    }
+   
+}
+
+/*
+Description:
+   Update the input reg in reservatin station with
+   1. corrresponding tag(instr) if not ready
+   2. NULL if ready
+*/ 
+void update_rs_entry(instruction_t* currInstr){
+   for(int i = 0; i < NUM_INPUT_REGS; i++){
+      // NOTE: map_table[currInstr -> r_in[i]] might be NULL
+      // if NULL: that means it's ready for issue
+      // else: record the value for RAW
+      if(currInstr -> r_in[i] != DNA){
+         currInstr -> Q[i] = map_table[currInstr->r_in[i]];
+      }
+   }   
+}
+
+/*
+/*
+Description:
+   Update the output reg in map table with it's corresponding instruction
+*/
+void update_maptable(instruction_t* currInstr){
+   for(int i = 0; i < NUM_OUTPUT_REGS; i++){
+      if(currInstr -> r_out[i] != DNA)
+         map_table[currInstr -> r_out] = currInstr;
+   }
+}
+
+
+/* 
+ * Description: 
+ * 	Calls fetch and dispatches an instruction at the same cycle (if possible)
+ * Inputs:
+ *      trace: instruction trace with all the instructions executed
+ * 	current_cycle: the cycle we are at
+ * Returns:
+ * 	None
+ */
+// ENTER DISPATCH STAGE
+// Main purpose: 
+void fetch_To_dispatch(instruction_trace_t* trace, int current_cycle) {
+   /* ECE552: YOUR CODE GOES HERE */
+   // case where no instruction in instruction queue: can't dispatch
+   if (IFQ -> head == IFQ -> tail){
+      return;
+   }
+ 
+
+   /*
+   A fetched instruction can be dispatched (complete D) immediately if a reservation
+   station entry will be available in the next cycle. Structural hazards on reservation
+   stations are resolved here with stalls.
+   */
+   bool D_success = false;
+   instruction_t* currInstr = IFQ->head->data;
+   // check for structural hazard
+
+   md_opcode currOp = currInstr->op;
+
+   //OoO issue
+   //check from head to tail: the first available dispatched && has RS available
+
+
+   if(IS_UNCOND_CTRL(currOp) || IS_COND_CTRL(currOp)){
+      D_success = true;
+      currInstr->tom_dispatch_cycle = current_cycle;
+   }else if (USES_INT_FU(currOp)) {
+      //INT
+      instruction_t *insertPlace = NULL;
+      // returns true if avail entry and entryToInsert is where to insert
+      // returns false if no entry avail
+      D_success = getFreeReservationEntry(reservINT, RESERV_INT_SIZE, insertPlace);
+      if(D_success){
+         insertPlace = currInstr;
+         currInstr->tom_dispatch_cycle = current_cycle;
+         update_rs_entry(currInstr);
+         update_maptable(currInstr);
+      }
+   } else if (USES_FP_FU(currOp)) {
+      //FP
+      instruction_t *insertPlace = NULL;
+      // returns true if avail entry and entryToInsert is where to insert
+      // returns false if no entry avail
+      D_success = getFreeReservationEntry(reservFP, RESERV_FP_SIZE, insertPlace);
+      if(D_success){
+         insertPlace = currInstr;
+         currInstr->tom_dispatch_cycle = current_cycle;
+         update_rs_entry(currInstr);
+         update_maptable(currInstr);
+      } 
+   }
+   
+   // remove instruction from queue
+   if(D_success)
+      queuePop(IFQ);
+   // if !D_success: stall so don't pop from queue cuz wanna retry when reservation
+   // station needed is ready so don't update rs entry nor map table
 }
 
 /* 
